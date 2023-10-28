9f97dde0fee69f701fc0265ff9e22e7ebb2a6860
Jagdish Kamadi <jagdish.m.kamadi@gmail.com>
2023-10-28 13:53:03 +0530

    Removing the project in next commit

--- Development Programs/Microservice Demos/Teluxo Microservices Demos/quizapp/quizapp/src/main/java/com/epam/quizapp/repository/QuizRepository.java
+++
@@ -1,9 +0,0 @@
-package com.epam.quizapp.repository;
-
-import com.epam.quizapp.model.Quiz;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizRepository extends JpaRepository<Quiz, Integer> {
-
-    Quiz findByQuizTitle(String quizTitle);
-}
